b125cccd72d495f74b8e32e833ff8f7e074a819a
Vignesh12-12 <jackyvicky66@gmail.com>
2024-12-22 21:47:13 +0530

    Added Simple_interest.java

---
+++ Simple_interest.java
@@ -0,0 +1,13 @@
+/*1. Declare and assign necessary variables and find simple interest. Formula
+to find SI is given below:*/
+
+package topic0;
+public class Example {
+	public static void main(String[] args) {
+		int p=355000;
+		byte n=12;
+		float r=15;
+		double SI=(p*n*r)/100;
+		System.out.println(SI);
+	}
+}
