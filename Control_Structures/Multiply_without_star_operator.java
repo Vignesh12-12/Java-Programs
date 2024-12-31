1aa51c8be64088e2eafc8bed9a0483b0dd27ee1d
Vignesh12-12 <jackyvicky66@gmail.com>
2024-12-31 11:49:20 +0530

    Added new program

---
+++ Control_Structures/Multiply_without_star_operator.java
@@ -0,0 +1,13 @@
+// Read 2 numbers and multiply without using * operator.
+
+// Program:
+public class Multiply {
+	public static void main(String[] args) {
+		int num1=7,num2=5,sum=0;
+		while(num2>=1) {
+			sum=sum+num1;
+			num2--;
+		}
+		System.out.println(sum);
+	}
+}
