f499adec503640c3bdede84378ac9de86a01edd7
Vignesh12-12 <jackyvicky66@gmail.com>
2025-01-02 12:04:35 +0530

    Added new program

---
+++ Array/Print_Reverse_in_Array.java
@@ -0,0 +1,12 @@
+// Reverse the Array
+// Eg : a={1,4,5,3,2}.
+// Program:
+
+package topic0;
+public class Array3 {
+	public static void main(String[] args) {
+		int[] a= {1,4,5,3,2};
+		for(int i=a.length-1;i>=0;i--)
+			System.out.println(a[i]);
+	}
+}
